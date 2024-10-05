f5e9c893127878238cda44f2d421744378cec6f2
Jaime-Sanz <118962371+Jaime-Sanz@users.noreply.github.com>
2024-10-04 18:53:48 -0700

    updated css
    added background and sidebar css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{ejs,js}"], 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+      backdropBlur: {
+        '5.5px': '5.5px',
+      },
+      boxShadow: {
+        'custom-shadow': '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
+      },
+    },
   },
   plugins: [],
 }
